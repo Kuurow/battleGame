2c7253e57b41785c4c487dce9b229e0ed7fd885c
Kuurow <kuuroww@gmail.com>
2020-04-21 15:37:13 +0200

    [UPDATE / FIXES] Ajout des fonctions SETTER et GETTER pour chaque attributs, mise en place d'un constructeur pour la classe Personnages et début de gestion d'un petit combat.

--- index.php
+++ index.php
@@ -1,25 +1,111 @@
 <?php
 
-class Personnage {
-    private $_force;  //--- Attributs avec $_ 
-    private $_localisation;  //--- Attributs avec $_ 
-    private $_experience = 50;  //--- Attributs avec $_ 
-    private $_degats;  //--- Attributs avec $_ 
+class Personnage
+{
+    private $_force = 20; //--- Attributs avec $_
+    private $_localisation; //--- Attributs avec $_
+    private $_experience = 0; //--- Attributs avec $_
+    private $_vie;
+    private $_nom;
 
-    public function parler() {
-        print("Je suis un personnage !");
+    public function __construct($nom, $force = 10, $experience = 0, $vie = 1000)
+    {
+        $this->setNom($nom);
+        $this->setForce($force);
+        $this->setExperience($experience);
+        $this->setVie($vie);
+    }
+    //--- SETTER & GETTER pour l'attribut _nom
+    public function setNom($nom)
+    {
+        if (!is_string($nom)) {
+            trigger_error("Veuillez saisir le nom du personnage avec un format texte.", E_USER_WARNING);
+        } else {
+            $this->_nom = $nom;
+        }
     }
 
+    public function getNom()
+    {
+        return $this->_nom;
+    }
+    //--- SETTER & GETTER pour l'attribut _force
+    public function setForce($force)
+    {
+        if (!is_int($force)) {
+            trigger_error("Veuillez saisir une force avec un format entier.", E_USER_WARNING);
+        } else {
+            $this->_force = $force;
+        }
+    }
+
+    public function getForce()
+    {
+        return $this->_force;
+    }
+    //--- SETTER & GETTER pour l'attribut _experience
+    public function setExperience($experience)
+    {
+        if (!is_int($experience)) {
+            trigger_error("Veuillez saisir une experience avec un format entier.", E_USER_WARNING);
+        } else {
+            $this->_experience = $experience;
+        }
+    }
+
+    public function getExperience()
+    {
+        return $this->_experience;
+    }
+    //--- SETTER & GETTER pour l'attribut _vie
+    public function setVie($vie)
+    {
+        if (!is_int($vie)) {
+            trigger_error("Veuillez saisir nombre de points de vie avec un format entier.", E_USER_WARNING);
+        } else {
+            $this->_vie = $vie;
+        }
+    }
+
+    public function getVie()
+    {
+        return $this->_vie;
+    }
+
+    //--- Combat entre deux personnages par l'action frapper
+    public function frapper(Personnage $ennemi)
+    {
+        print($this->getNom() . " à frappé " . $ennemi->getNom() . "<br />");
+        $vieEnnemiAvant = $ennemi->getVie();
+        $ennemi->_vie -= $this->_force;
+        $this->_experience += 1;
+        $this->getExperience();
+        print($ennemi->getNom() . " a perdu " . ($vieEnnemiAvant - $ennemi->getVie()) . " point(s) de vie. <br />");
+        $this->resultFight($ennemi);
+    }
+    //--- Affichage de la vie d'un personnage
+    public function afficherVie()
+    {
+        print("Vie du personnage " . $this->getNom() . " : " . $this->getVie());
+    }
+    //--- Affichage des points d'expérience d'un personnage
     public function afficherExperience() {
-        print($this->_experience);
+        print("Le personnage " . $this->_nom . " a " . $this->getExperience() . " point(s) d'expérience. <br/>");
     }
-
-    public function gagnerExperience($gainExperience) {
-        print("<br/>" . ($this->_experience + $gainExperience));
+    //--- Résultats suite à un combat
+    public function resultFight($ennemi)
+    {
+        print("<b>Résultats du combat :</b><br />");
+        print($this->getNom() . ": Vie = " . $this->getVie() . " / Force : " . $this->getForce() . " / Expérience : " . $this->getExperience() . "<br />"); //--- Affichage des stats de l'attaquant
+        print($ennemi->getNom() . ": Vie = " . $ennemi->getVie() . " / Force : " . $ennemi->getForce() . " / Expérience : " . $ennemi->getExperience() . "<br />"); //--- Affichage des stats de l'ennemi
     }
 
 }
-$perso = new Personnage();
-$perso->afficherExperience();
-$perso->gagnerExperience(10);
-?>
+
+//-------------------------------------------------------------------------------------------------------------------------
+print("<h1>PHP BattleGame</h1><hr />");
+$perso1 = new Personnage("Paul");
+$perso2 = new Personnage("Pierre");
+$perso1->afficherExperience();
+$perso2->afficherExperience();
+$perso1->frapper($perso2);
